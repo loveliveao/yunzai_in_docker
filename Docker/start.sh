dea179359ffa9aa6ebdb2a5639a2fdb2af35ba42
loveliveao <loveliveaowu@gmail.com>
2024-09-12 22:15:48 +0800

    v2.0.1

--- Docker/start.sh
+++ Docker/start.sh
@@ -5,51 +5,56 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # 无颜色
 
+# 函数：输出带颜色的消息
+print_message() {
+    local color=$1
+    local message=$2
+    echo -e "${color}${message}${NC}"
+}
+
 # 提示用户脚本正在初始化
-echo -e "${RED}正在初始化 Yunzai，请勿退出...${NC}"
+print_message "$RED" "正在初始化 Yunzai，请勿退出..."
 
 # 启动 Redis 服务器
-echo -e "${GREEN}启动 Redis 服务器...${NC}"
+print_message "$GREEN" "启动 Redis 服务器..."
 redis-server --daemonize yes &> /dev/null
 
 # 检查 Yunzai 目录是否存在且不为空
-echo -e "${RED}正在检查 Yunzai 目录是否存在且不为空...${NC}"
+print_message "$RED" "正在检查 Yunzai 目录是否存在且不为空..."
 if [ ! -d "/Yunzai" ] || [ -z "$(ls -A /Yunzai)" ]; then
-    echo -e "${GREEN}Yunzai 目录不存在或为空，正在克隆 Yunzai ...${NC}"
+    print_message "$GREEN" "Yunzai 目录不存在或为空，正在克隆 Yunzai ..."
 
     # 克隆 Yunzai
     git clone https://gitee.com/TimeRainStarSky/Yunzai /Yunzai
-    echo -e "${GREEN}Yunzai 克隆完成。${NC}"
-    cd /Yunzai
+    print_message "$GREEN" "Yunzai 克隆完成。"
+    cd /Yunzai || exit
 
-    # 注释掉克隆插件的部分
-    echo -e "${GREEN}正在克隆插件...${NC}"
+    # 克隆插件
+    print_message "$GREEN" "正在克隆插件..."
     git clone --depth 1 https://gitee.com/yoimiya-kokomi/miao-plugin plugins/miao-plugin
     git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-genshin.git plugins/genshin
     git clone --depth 1 https://gitee.com/guoba-yunzai/guoba-plugin.git plugins/Guoba-Plugin
     git clone --depth 1 https://gitee.com/kyrzy0416/rconsole-plugin.git plugins/rconsole-plugin
 
-    # 安装 Node.js 依赖
-    echo -e "${GREEN}正在安装 Node.js 依赖...${NC}"
+    # 安装依赖
+    print_message "$GREEN" "正在安装 Node.js 依赖..."
     npm --registry=https://registry.npmmirror.com install pnpm -g
     pnpm config set registry https://registry.npmmirror.com
     pnpm install -P
-    echo -e "${GREEN}Node.js 依赖安装完成。${NC}"
+    print_message "$GREEN" "Node.js 依赖安装完成。"
 
-    # 安装全局的 Freyr 工具
-    echo -e "${GREEN}正在安装全局 Freyr 工具...${NC}"
+    print_message "$GREEN" "正在安装全局 Freyr 工具..."
     npm install -g freyr
-    echo -e "${GREEN}Freyr 安装完成。${NC}"
+    print_message "$GREEN" "Freyr 安装完成。"
 
-    # 安装 Python 依赖
-    echo -e "${GREEN}正在安装 Python 依赖 yt-dlp...${NC}"
-    pip install yt-dlp
-    echo -e "${GREEN}Python 依赖安装完成。${NC}"
+    print_message "$GREEN" "正在安装 Python 依赖 yt-dlp..."
+    pip3 install yt-dlp
+    print_message "$GREEN" "Python 依赖安装完成。"
 else
-    echo -e "${RED}Yunzai 目录已存在且不为空，将跳过克隆和安装依赖步骤...${NC}"
-    cd /Yunzai
+    print_message "$RED" "Yunzai 目录已存在且不为空，将跳过克隆和安装依赖步骤..."
+    cd /Yunzai || exit
 fi
 
 # 启动 Yunzai 并在前台运行
-echo -e "${RED}即将启动 Yunzai...${NC}"
+print_message "$RED" "即将启动 Yunzai..."
 node .
